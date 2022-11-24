f1f9efd0e5b5a11fd7dbb054aec4b07cbc67864c
sangkyun <sangkyun@LAPTOP-U264MDMD>
2022-11-24 00:36:13 -0800

    Jenkins test

---
+++ src/main/java/kr/co/mlec/Board/BoardController.java
@@ -0,0 +1,10 @@
+package kr.co.mlec.Board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+}
